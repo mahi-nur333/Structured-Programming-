7bebf18489fcc8f346ced280757c52e3dde19e3b
Mahinur Rahman Pamel <142707968+mahi-nur333@users.noreply.github.com>
2024-02-09 02:33:37 +0600

    Update limitation.c

--- Module-1 Basic Syntex,Variable & Data types/limitation.c
+++ Module-1 Basic Syntex,Variable & Data types/limitation.c
@@ -3,7 +3,7 @@ int main(){
  //int a =1000000000; more than +-10^9 will not acepted
  //printf("%d",a);
  
- //long long int a=100000000000000; more than +-10^ will not acepted
+ //long long int a=100000000000000; more than +-10^18 will not acepted
  //printf("%lld",a);
 
   //float a= 25.556789; after decimal point 6 digit can take
