9299ba2c95d2887597b989a865509b5ea5ddc9eb
Mahinur Rahman Pamel <142707968+mahi-nur333@users.noreply.github.com>
2024-02-09 02:31:20 +0600

    Add files via upload

---
+++ Module-1 Basic Syntex,Variable & Data types/limitation.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+ //int a =1000000000; more than +-10^9 will not acepted
+ //printf("%d",a);
+ 
+ //long long int a=100000000000000; more than +-10^ will not acepted
+ //printf("%lld",a);
+
+  //float a= 25.556789; after decimal point 6 digit can take
+  //printf("%f",a);
+
+  double a=5.556789058864769; //after decimal point 15 digit can take 
+  printf("%0.15lf",a);
+
+}
