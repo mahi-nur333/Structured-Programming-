523583bfdadb0d1a1907523c71a8cbc72e84672a
Mahinur Rahman Pamel <142707968+mahi-nur333@users.noreply.github.com>
2024-02-09 19:09:29 +0600

    Update arithmetic.c

--- Module-2 Operator and Conditional Statement/arithmetic.c
+++ Module-2 Operator and Conditional Statement/arithmetic.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main() {
 int a=20,b=3;
-//int sum=a+b;
-//int mns=a-b;
-//int mlt=a*b;
-//int dvd=a/b;
-int mod=a%b;
+//int sum=a+b; uses for +
+//int mns=a-b; uses for -
+//int mlt=a*b;  uses for *
+//int dvd=a/b;  uses for /
+int mod=a%b;    //uses for mod %
 printf("%d" ,mod);
 return 0;
 
