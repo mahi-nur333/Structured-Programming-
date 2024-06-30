5f32338d83c70d71260e9a50a108493c147c17bd
Mahinur Rahman Pamel <142707968+mahi-nur333@users.noreply.github.com>
2024-06-30 19:50:01 +0600

    Add files via upload

---
+++ Module-4 Assginment 1/devisible.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+        if ( N % 3==0)
+        {
+            printf("YES", N);
+        }
+        else
+        {
+            printf("NO", N);
+        }
+    
+    return 0;
+}
